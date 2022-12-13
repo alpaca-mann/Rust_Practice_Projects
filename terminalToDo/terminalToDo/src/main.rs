5f32a8d45a72fb94491a2ba52dc4cfa0f51cea87
Alpaca man <johnerhover@gmail.com>
2022-12-14 02:58:09 +0900

    asking for user input -testing-

--- terminalToDo/terminalToDo/src/main.rs
+++ terminalToDo/terminalToDo/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+//loop {
+   println!("Add your ToDo");
+
+   let mut new_todo:String = String::new();
+
+   io::stdin()
+       .read_line(&mut new_todo)
+       .expect("Sorry, try again.");
+
+    
+    println!("Do you wish to add more? y = Yes. n = No.");
+    //let mut test:String = String::new();
+//}   
 }
